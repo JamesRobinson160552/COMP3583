593c44921f23fc9bb218ace420c5d5e2ea7aeeff
James Robinson <160552r@acadiau.ca>
2023-10-28 19:17:50 -0300

    Some formatting on results page

--- SourceCode/frontend/src/views/SearchResultsView.vue
+++ SourceCode/frontend/src/views/SearchResultsView.vue
@@ -9,13 +9,13 @@
     </div>
     <div class="searchresults">
         <h1 v-if="{ term }.length > 1">Results for "{{ term }}"</h1>
-        <div v-for="result in searchresults" v-bind:key="result.id" :class="{ reliable: result.rating >= 4, okay: result.rating >= 3 && result.rating < 4, unreliable: result.rating < 3 }">
-            <div class="search-result">
+        <div v-for="result in searchresults" v-bind:key="result.id" class="search-result">
+            <div :class="{ reliable: result.rating >= 4, okay: result.rating >= 3 && result.rating < 4, unreliable: result.rating < 3 }">
                 <h3> {{ result.name }} </h3>
                 <img v-bind:src="result.imagepath">
                 <p> {{ result.opentime }} - {{ result.closetime }} </p>
                 <p>Reliability Rating: {{ result.rating }}</p>
-                <router-link :to="`/businessdetails/${result.id}`">Details</router-link>
+                <button class="details-button"><router-link :to="`/businessdetails/${result.id}`">Details</router-link></button>
             </div>
         </div>
     </div>
@@ -42,11 +42,46 @@ function search(term = '') {
 <style scoped>
     .reliable {
         background-color: lightgreen;
+        border: 1px solid black;
+        border-radius: 10px;
+        padding: 10px;
     }
+
     .okay {
         background-color: lightyellow;
+        border: 1px solid black;
+        border-radius: 10px;
+        padding: 10px;
     }
+
     .unreliable {
         background-color: lightsalmon;
+        border: 1px solid black;
+        border-radius: 10px;
+        padding: 10px;
+    }
+
+    .searchresults {
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-template-rows: 1fr;
+        gap: 10px;
+        padding: 100px;
+        margin: auto;
+        grid-template-rows: repeat(3, 1fr); /* Equal height rows */
+    }
+
+    .details-button {
+        background-color: #f1f1f1;
+        border: none;
+        border-radius: 36px;
+        color: black;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
     }
 </style>
