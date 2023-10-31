5f2eeddb9a4be6ab241c7ea89f56c4eb06db96b2
NoGoRoGo <rogowskys@gmail.com>
2023-10-31 18:29:43 -0300

    Update vue.config.js to allow for external network connections.

--- SourceCode/frontend/vue.config.js
+++ SourceCode/frontend/vue.config.js
@@ -2,3 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+    historyApiFallback: true,
+    allowedHosts: 'all',
+  },
+}
