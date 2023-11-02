114713e29a3df3e22df19b00cdd6a694b9fa0fba
R-Hambly-TS <55469422+R-Hambly-TS@users.noreply.github.com>
2023-11-02 20:07:08 -0300

    Added in 9 additional businesses

--- SourceCode/frontend/src/fakedata.js
+++ SourceCode/frontend/src/fakedata.js
@@ -31,4 +31,94 @@ export const businesses = [
         imagepath: '../assets/subway.jpg',
         description: `Subway, a globally recognized fast-food chain, is celebrated for its fresh and customizable sandwiches. With a presence in countless countries, Subway distinguishes itself with its commitment to providing healthier and personalized options. Customers can select from an array of freshly baked bread, fillings, and toppings, ensuring a tailored, wholesome meal. The restaurant's "Build Your Own" concept offers endless combinations, appealing to various tastes and dietary preferences. Subway's clean, contemporary interiors and efficient service create a welcoming atmosphere for quick and nutritious dining. For those seeking a delicious and nutritious alternative to traditional fast food, Subway remains a top choice.`
     },
+    {
+        id: 4,
+        name: 'Dairy Queen',
+        address: '9071 Commercial Drive',
+        opentime: '11:00 AM',
+        closetime: '10:00 PM',
+        rating: 5.0,
+        imagepath: '../assets/dairyqueen.jpg',
+        description: `Dairy Queen, often abbreviated as "DQ," is a popular American chain of fast-food restaurants and ice cream parlors. Dairy Queen is renowned for its diverse menu, which includes a variety of ice cream treats and fast-food items.`
+    },
+    {
+        id: 5,
+        name: 'Canadian Tire',
+        address: '9212 Commercial St',
+        opentime: '9:00 AM',
+        closetime: '7:00 PM',
+        rating: 3.0,
+        imagepath: '../assets/canadiantire.jpg',
+        description: `Canadian Tire is a Canadian retail company known for its wide range of products, including automotive supplies, sporting goods, home and kitchen essentials, and outdoor equipment. It also operates a loyalty program called the Canadian Tire Money, where customers can earn and redeem rewards on their purchases.`
+    },
+    {
+        id: 6,
+        name: 'Value Village',
+        address: '9256 Commercial St',
+        opentime: '7:00 AM',
+        closetime: '5:00 PM',
+        rating: 2.0,
+        imagepath: '../assets/valuevillage.jpg',
+        description: `Value Village is a thrift store chain with locations in the United States and Canada that specializes in selling secondhand clothing, accessories, household items, and more at affordable prices. The stores often partner with nonprofit organizations and have a focus on sustainability and recycling by offering pre-owned items to customers.`
+    },
+    {
+        id: 7,
+        name: 'KenWo Golf Club',
+        address: '9514 Commercial St',
+        opentime: '12:00 PM',
+        closetime: '5:00 PM',
+        rating: 1.5,
+        imagepath: '../assets/kenwo.jpg',
+        description: `The KenWo Golf Club is a prestigious golf course located in New Minas, Nova Scotia, Canada. Known for its scenic beauty and challenging layout, KenWo Golf Club offers golf enthusiasts a unique and enjoyable experience with well-maintained fairways, lush greens, and stunning views of the surrounding landscape, making it a popular destination for golfers in the region.`
+    },
+    {
+        id: 8,
+        name: 'Al Whittle Theatre',
+        address: '450 Main St',
+        opentime: '11:00 AM',
+        closetime: '10:00 PM',
+        rating: 3.5,
+        imagepath: '../assets/alwhittle.jpg',
+        description: `The Al Whittle Theatre is a cultural and entertainment venue located in Wolfville, Nova Scotia, Canada. Housed in a historic building, the theater serves as a hub for the arts and film in the community. It regularly hosts a variety of events, including film screenings, live performances, concerts, and other cultural activities, making it a central part of Wolfville's cultural scene. The Al Whittle Theatre is named after Al Whittle, a local musician and actor, and it continues to play a vital role in promoting and showcasing the arts in the region.`
+    },
+    {
+        id: 9,
+        name: 'Perkins Cafe',
+        address: '15 Horton Ave',
+        opentime: '9:00 AM',
+        closetime: '6:00 PM',
+        rating: 3.0,
+        imagepath: '../assets/perkins.png.webp',
+        description: `If you're on the hunt for a cool hangout spot with a cozy coffee shop vibe, look no further than the SUB! Get ready to dive into the scrumptious world of our famous grilled paninis, and wash it all down with the most refreshing fruit smoothies in town. Our coffee? It's not just any coffee; it's Just Us Coffee! Plus, we've got a treasure trove of sweet treats waiting for you.`
+    },
+    {
+        id: 10,
+        name: 'Tim Hortons',
+        address: '370 Main Street',
+        opentime: '6:00 AM',
+        closetime: '9:00 PM',
+        rating: 5.0,
+        imagepath: '../assets/timhortons.png',
+        description: `Tim Hortons is a well-known Canadian fast-food restaurant and coffee shop chain with a strong presence in Canada and other countries. Tim Hortons holds a special place in Canadian culture, and it is a go-to destination for many people seeking a quick meal, coffee, or snack. The brand is often associated with a sense of community and a warm, inviting atmosphere.`
+    },
+    {
+        id: 11,
+        name: 'NSLC Wolfville',
+        address: '122 Front St',
+        opentime: '10:00 AM',
+        closetime: '9:00 PM',
+        rating: 3.0,
+        imagepath: '../assets/nslc.jpg.webp',
+        description: `The Nova Scotia Liquor Corporation (NSLC) is the government-owned agency responsible for the retail sale and distribution of alcoholic beverages in the Canadian province of Nova Scotia. Customers must be of legal drinking age to purchase alcohol, and there are restrictions on where and when alcohol can be consumed.`
+    },
+    {
+        id: 12,
+        name: 'Annapolis Cider Company',
+        address: '388 Main St',
+        opentime: '10:00 AM',
+        closetime: '7:00 PM',
+        rating: 5.0,
+        imagepath: '../assets/annapolisciderco.jpg',
+        description: `Annapolis Cider Company is a craft cidery located in Wolfville, Nova Scotia, Canada. This cidery is known for producing a variety of artisanal ciders made from locally sourced apples and other high-quality ingredients. The Annapolis Cider Company is committed to crafting small-batch, premium ciders that showcase the rich agricultural heritage of the Annapolis Valley region.`
+    },
 ]
